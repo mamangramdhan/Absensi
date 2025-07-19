7abeade60781dffb5f6b3b20af3b3a3142597357
mamangramdhan <mamangramdhan@gmail.com>
2025-07-20 00:02:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("camera");
 const canvas = document.getElementById("canvas");
 const captureBtn = document.getElementById("captureBtn");
 
-async function startCamera() {
+startBtn.onclick = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
+
+    // Tampilkan elemen setelah kamera aktif
+    startBtn.style.display = "none";
+    video.style.display = "block";
+    captureBtn.style.display = "inline-block";
   } catch (err) {
     alert("Gagal mengakses kamera: " + err.message);
   }
-}
+};
 
 captureBtn.onclick = () => {
   const context = canvas.getContext("2d");
@@ -19,7 +25,6 @@ captureBtn.onclick = () => {
 
   const imageData = canvas.toDataURL("image/jpeg", 0.9);
 
-  // Kirim base64 ke GAS backend
   fetch("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec", {
     method: "POST",
     body: JSON.stringify({ image: imageData }),
@@ -29,5 +34,3 @@ captureBtn.onclick = () => {
   .then(msg => alert(msg))
   .catch(err => alert("Gagal kirim gambar: " + err.message));
 };
-
-startCamera();
