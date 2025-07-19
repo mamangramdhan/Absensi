ec021480dd126ed4f15780c5ef1c5b6f4b711e9d
mamangramdhan <mamangramdhan@gmail.com>
2025-07-20 00:53:10 +0700

    Update script.js
    SCRIPT JOSS

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 const video = document.getElementById("camera");
 const canvas = document.getElementById("canvas");
 const captureBtn = document.getElementById("captureBtn");
-const reloadBtn = document.getElementById("reloadBtn");
 
 async function startCamera() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
-    video.setAttribute("playsinline", true);
-    video.onloadedmetadata = () => {
-      video.play();
-    };
-    reloadBtn.style.display = "none";
   } catch (err) {
-    console.error("Gagal membuka kamera:", err);
-    alert("Gagal membuka kamera. Klik untuk mencoba lagi.");
-    reloadBtn.style.display = "block";
+    alert("Gagal mengakses kamera: " + err.message);
   }
 }
 
@@ -27,18 +19,15 @@ captureBtn.onclick = () => {
 
   const imageData = canvas.toDataURL("image/jpeg", 0.9);
 
+  // Kirim base64 ke GAS backend
   fetch("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec", {
     method: "POST",
     body: JSON.stringify({ image: imageData }),
     headers: { "Content-Type": "application/json" }
   })
-    .then(res => res.text())
-    .then(msg => alert(msg))
-    .catch(err => alert("Gagal kirim gambar: " + err.message));
+  .then(res => res.text())
+  .then(msg => alert(msg))
+  .catch(err => alert("Gagal kirim gambar: " + err.message));
 };
 
-reloadBtn.onclick = () => {
-  startCamera(); // Coba ulang kamera
-};
-
-startCamera(); // Mulai otomatis
+startCamera();
