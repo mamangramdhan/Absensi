772de56be43589095248830f12c886f0e883f018
mamangramdhan <mamangramdhan@gmail.com>
2025-07-20 10:43:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ const video = document.getElementById("camera");
 const canvas = document.getElementById("canvas");
 const captureBtn = document.getElementById("captureBtn");
 
-const tg = window.Telegram.WebApp;
-tg.expand();
-
 async function startCamera() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
   } catch (err) {
-    alert("Gagal membuka kamera: " + err.message);
+    alert("Gagal mengakses kamera: " + err.message);
   }
 }
 
@@ -20,8 +17,17 @@ captureBtn.onclick = () => {
   canvas.height = video.videoHeight;
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  const imageData = canvas.toDataURL("image/jpeg", 0.9); // base64
-  tg.sendData(imageData); // kirim ke bot
+  const imageData = canvas.toDataURL("image/jpeg", 0.9);
+
+  // Kirim base64 ke GAS backend
+  fetch("https://script.google.com/macros/s/AKfycbzkkVrrzB1NOQTs2Uz39YNLE44vPlRp1ITheRTXNqz9pfNJ1KsP_FPPTp1e3qBbWLJs/exec", {
+    method: "POST",
+    body: JSON.stringify({ image: imageData }),
+    headers: { "Content-Type": "application/json" }
+  })
+  .then(res => res.text())
+  .then(msg => alert(msg))
+  .catch(err => alert("Gagal kirim gambar: " + err.message));
 };
 
 startCamera();
